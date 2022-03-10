7256f6d52ca7dd7217eb6a9787d8166bcb71e19e
robots4life <34193243+robots4life@users.noreply.github.com>
2022-03-11 00:36:32 +0100

    create todoStore & addTodo function

---
+++ src/stores/todoStore.js
@@ -0,0 +1,17 @@
+import { writable } from 'svelte/store';
+
+export const todos = writable([]); // create an empty array of todos in the store that can be written to
+
+export const addTodo = (text) => {
+	//
+	// update the todos store with the update method
+	todos.update((currentValue) => {
+		//
+		// update the array and re-assign it to todos
+		// spread out the currentValue and add in the new value
+		const newTodos = [...currentValue, { text, completed: false, id: Date.now() }];
+
+		// return the newly created array
+		return newTodos;
+	});
+};
