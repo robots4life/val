8c089ebd65fa379c52920fec7aff93f4523e66db
robots4life <34193243+robots4life@users.noreply.github.com>
2022-03-15 17:48:38 +0100

    update Todo completed state

--- src/lib/Todo.svelte
+++ src/lib/Todo.svelte
@@ -1,18 +1,26 @@
 <script>
 	// import the deleteTodo function from the store
-	import { deleteTodo } from '../stores/todoStore.js';
+	import { deleteTodo, toggleTodoCompleted } from '../stores/todoStore.js';
 
 	// Allow this component to accept values from the outside
 	export let todo;
 </script>
 
 <li class="text-2xl list-none bg-sky-200 rounded-lg px-6 py-8 my-6">
-	text: {todo.text} | index: {todo.id}
+	<input
+		name="completed"
+		type="checkbox"
+		checked="{todo.completed}"
+		on:change="{() => toggleTodoCompleted(todo.id, todo.completed)}"
+		class="mr-2 form-checkbox h-5 w-5"
+	/>
+
+	text: {todo.text} | index: {todo.id} | completed: {todo.completed}
 
 	<!--
 	Check out below REPL to understand why we have to use an arrow function syntax
 	for the deleteTodo(todo.id) click handler
 	https://svelte.dev/repl/91499094927146a8a0dfa4a949247216?version=3.46.4
 	-->
-	<button on:click="{() => deleteTodo(todo.id)}">delete Todo</button>
+	<button on:click="{() => deleteTodo(todo.id)}">| Delete Todo</button>
 </li>
--- src/stores/todoStore.js
+++ src/stores/todoStore.js
@@ -40,3 +40,57 @@ export const deleteTodo = (id) => {
 		return newTodos;
 	});
 };
+
+// https://stackoverflow.com/a/13964186
+// const data = [
+// 	{ text: 'A', completed: false, id: 1647351834882 },
+// 	{ text: 'B', completed: false, id: 1647351837560 },
+// 	{ text: 'C', completed: false, id: 1647351840585 }
+// ];
+
+// const result = data.filter((element) => {
+// 	return element.id === 1647351837560;
+// });
+// console.log(result);
+
+// Toggle Todo Completed
+export const toggleTodoCompleted = (id, completed) => {
+	// console.log('completeTodo(id) called');
+	// console.log(id);
+	// console.log(completed);
+
+	// todos.update((currentValue) => {
+	// 	console.log(currentValue);
+
+	// 	const todoCompleted = currentValue.filter((element) => {
+	// 		//
+	// 		// filter the todo with the todo.id that is passed into the function
+	// 		return () => {
+	// 			console.log('inside filter');
+	// 			if (element.id === id) {
+	// 				console.log(element.completed);
+	// 				element.completed = true;
+	// 			}
+	// 		};
+	// 	});
+	// 	// console.log(todoCompleted[0].completed);
+	// 	// todoCompleted[0].completed = !todoCompleted[0].completed;
+	// 	// console.log(todoCompleted[0].completed);
+
+	// 	return todoCompleted;
+	// });
+
+	todos.update((todos) => {
+		let index = -1;
+		for (let i = 0; i < todos.length; i++) {
+			if (todos[i].id === id) {
+				index = i;
+				break;
+			}
+		}
+		if (index !== -1) {
+			todos[index].completed = !todos[index].completed;
+		}
+		return todos;
+	});
+};
